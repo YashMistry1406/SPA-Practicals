012150b9f9e2eaf03cf278f00190bc1814a51c9c
Keshav Mishra <47028747+mishrakeshav@users.noreply.github.com>
2019-04-26 18:02:21 +0530

    Add files via upload

---
+++ 27.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int i, j, n=5;
+    for(i=0; i<n; i++)
+    {
+        for(j=0; j<i; j++)
+            printf(" ");
+        for(j=n; j>i; j--)
+            printf("*");
+
+        printf("\n");
+    }
+}
+/*Output:
+*****
+ ****
+  ***
+   **
+    *
+*/
