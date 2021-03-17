867d05ffa07be9fbf27b68c38b0d59d319e1f86c
YashMistry1406 <yashmistry2000@gmail.com>
2021-03-17 19:16:45 +0530

    checking

--- 3_PrimeNumber.c
+++ 3_PrimeNumber.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+/*#include <stdio.h>
 int main()
 {
     int n, i, flag = 0;
@@ -29,4 +29,33 @@ int main()
 
 
     return 0;
+}*/
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("Enter the Number : ");
+	scanf("%d",&n);
+	int i,j,arr[n];
+	
+	for(i=1; i<=n; i++)
+	{
+		arr[i]=1;
+	}
+	
+	for(i=2; i*i<=n; i++)
+	{
+		if(arr[i]==1)
+		{
+			for(j=i*i; j<=n; j+=i)
+			{
+				arr[j]=0;
+			}
+		}
+	}
+	for(i=2; i<n; i++)
+	{
+		if(arr[i]==1)
+			printf("%d\t",i);
+	}	
 }
